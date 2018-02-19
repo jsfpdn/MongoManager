e26bc481ecf4fd025baebada0a8c97f6187bd4b6
Josef Podaný <pepsi@Josef-MacBook-Pro.local>
2018-02-19 18:04:41 +0100

    NEW: main.sh

---
+++ main.sh
@@ -0,0 +1,5 @@
+
+# 1. - check if mongodb is installed
+# 2. - get arguments from command line (amount of shards and replicas), always create mongos
+# 3. - parse arguments, evaluate them
+# 4. - make running v0.1 for starting shards
