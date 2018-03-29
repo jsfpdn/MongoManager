c1a17a68b79241169a8cdadce36fffc48116fd7e
Josef Podaný <josef.podany.ml@gmail.com>
2018-03-29 12:38:10 +0200

    FIX: gitignore file

--- main.sh
+++ main.sh
@@ -10,122 +10,122 @@ CONFIG_STARTING_PORT=28017  # port of first config server
 MONGOD_PORTS=()
 CONFIG_PORTS=()
 
-# Generates arrays of ports used later in the programme to start instances
-# $1 REPLICAS: integer  - number of mongod instances in one shard
-# $2 SHARDS: integer    - number of shards
-function generate_ports {
-    REPLICAS=$1
-    SHARDS=$2
+    # Generates arrays of ports used later in the programme to start instances
+    # $1 REPLICAS: integer  - number of mongod instances in one shard
+    # $2 SHARDS: integer    - number of shards
+    function generate_ports {
+        REPLICAS=$1
+        SHARDS=$2
 
-    case "$SHARDS" in   # Checking for valid amount of shards
-        0) TOTAL=$REPLICAS ;;
-        *) TOTAL=$((REPLICAS * SHARDS)) ;;
-    esac
+        case "$SHARDS" in   # Checking for valid amount of shards
+            0) TOTAL=$REPLICAS ;;
+            *) TOTAL=$((REPLICAS * SHARDS)) ;;
+        esac
 
-    for ((i = 0; i < TOTAL; i++)); do   # generating array of mongod ports
-        MONGOD_PORTS+=($(($MONGOD_STARTING_PORT + $i)))
-    done
-
-    if ((SHARDS < 2)); then # generating array of config ports
-        CONFIG_PORTS+=($CONFIG_STARTING_PORT)
-    else
-        for ((i = 0; i < SHARDS; i++)); do
-            CONFIG_PORTS+=($(($CONFIG_STARTING_PORT + $i)))
+        for ((i = 0; i < TOTAL; i++)); do   # generating array of mongod ports
+            MONGOD_PORTS+=($(($MONGOD_STARTING_PORT + $i)))
         done
-    fi
-}
 
-# Starts one replica set instance with port number and replica set ID
-# $1 ID: integer    - ID of group of replica sets
-# $2 PORT: integer  - port number of replica set
-function create_replica_set {
-    ID=$1
-    PORT=$2
+        if ((SHARDS < 2)); then # generating array of config ports
+            CONFIG_PORTS+=($CONFIG_STARTING_PORT)
+        else
+            for ((i = 0; i < SHARDS; i++)); do
+                CONFIG_PORTS+=($(($CONFIG_STARTING_PORT + $i)))
+            done
+        fi
+    }
 
-    # TODO: create folder for data
+    # Starts one replica set instance with port number and replica set ID
+    # $1 ID: integer    - ID of group of replica sets
+    # $2 PORT: integer  - port number of replica set
+    function create_replica_set {
+        ID=$1
+        PORT=$2
 
-    mongod --port $PORT --dbpath $DATADIR/$PORT --logpath $LOGPATH/$PORT.log --fork --replSet "rs${ID}" --shardsvr --smallfiles
-}
+        # TODO: create folder for data
 
-# Creates config string and adds it to replica set configuration
-# $1 ID: integer    - ID of group of replica set
-# $2 PORT: integer  - port number of replica set    
-function initialize_replica_set {
-    ID=$1
-    PORT=$2
+        mongod --port $PORT --dbpath $DATADIR/$PORT --logpath $LOGPATH/$PORT.log --fork --replSet "rs${ID}" --shardsvr --smallfiles
+    }
 
-    initString="rs.initiate({_id: 'rs${ID}', members: [{_id: ${ID}, host: 'localhost:${PORT}'}]}, {force : true})"
-    mongo --port $PORT --eval "$initString"
-}
+    # Creates config string and adds it to replica set configuration
+    # $1 ID: integer    - ID of group of replica set
+    # $2 PORT: integer  - port number of replica set    
+    function initialize_replica_set {
+        ID=$1
+        PORT=$2
 
-# adds another replica set to sharded cluster
-# $1 PRIMARY_PORT: integer  - port number of replica sets appropriate shard
-# $2 PORT: integer          - port number of replica set
-function config_replica_set {
-    PRIMARY_PORT=$1
-    PORT=$2
+        initString="rs.initiate({_id: 'rs${ID}', members: [{_id: ${ID}, host: 'localhost:${PORT}'}]}, {force : true})"
+        mongo --port $PORT --eval "$initString"
+    }
 
-    initString="rs.add('localhost:${PORT}')"
-    mongo --port $PRIMARY_PORT --eval "$initString"
-}
+    # adds another replica set to sharded cluster
+    # $1 PRIMARY_PORT: integer  - port number of replica sets appropriate shard
+    # $2 PORT: integer          - port number of replica set
+    function config_replica_set {
+        PRIMARY_PORT=$1
+        PORT=$2
 
-# starts new config server
-# $1 ID: integer    - port ID of config server
-# $2 PORT:          - port number of config server
-function start_config_server {
-    ID=$1
-    PORT=$2
+        initString="rs.add('localhost:${PORT}')"
+        mongo --port $PRIMARY_PORT --eval "$initString"
+    }
 
-    mongod --port $PORT --dbpath $DATADIR/${PORT} --configsvr --replSet configReplSet --fork --logpath $LOGPATH/$PORT.log --smallfiles
-}
+    # starts new config server
+    # $1 ID: integer    - port ID of config server
+    # $2 PORT:          - port number of config server
+    function start_config_server {
+        ID=$1
+        PORT=$2
+
+        mongod --port $PORT --dbpath $DATADIR/${PORT} --configsvr --replSet configReplSet --fork --logpath $LOGPATH/$PORT.log --smallfiles
+    }
 
 
 
-function extract_options {
-    # read all the options from command line
-    TEMP=`getopt -o m:r:s:p:l:d: --long mode:,replicas:,shards:,port:,logpath:,datapath: -n 'main.sh' "$@"`
-    eval set -- "$TEMP"
-}
+    function extract_options {
+        # read all the options from command line
+        TEMP=`getopt -o m:r:s:p:l:d: --long mode:,replicas:,shards:,port:,logpath:,datapath: -n 'main.sh' "$@"`
+        eval set -- "$TEMP"
+    }
 
-function show_help {
-    # Prints help
+    function show_help {
+        # Prints help
 
-    printf "usage:\n"
-}
+        printf "usage:\n"
+    }
 
-function create_dirs {
-    # Creates directories for logging and data
+    function create_dirs {
+        # Creates directories for logging and data
 
-    logpath=$0
-    datapath=$1
-}
+        logpath=$0
+        datapath=$1
+    }
 
-function start_replica_set {
-    # Starts one replica set on certain port    
+    function start_replica_set {
+        # Starts one replica set on certain port    
 
-    port=$0
-}
+        port=$0
+    }
 
-function create_pid_file {
-    # Creates one PID file on certain port for mongo instance
+    function create_pid_file {
+        # Creates one PID file on certain port for mongo instance
 
-    port=$0
-    type=$1
-}
+        port=$0
+        type=$1
+    }
 
-function create_init_string {
-    # Creates init string for sharding
-}
+    function create_init_string {
+        # Creates init string for sharding
+    }
 
-function kill_instance {
-    # Kills one instance on certain port and removes PID file
+    function kill_instance {
+        # Kills one instance on certain port and removes PID file
 
-    port=$0
-}
+        port=$0
+    }
 
-function stop_instance {
-    # Stops one instance
+    function stop_instance {
+        # Stops one instance
 
-    port=$0
-}
+        port=$0
+    }
 
